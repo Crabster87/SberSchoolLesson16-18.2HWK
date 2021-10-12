e40422f50ee1f2f4d55281e3d62384d9e94387b0
Crabster87 <iar876@yandex.ru>
2021-10-12 14:18:04 +0300

    countDownTimer

--- app/src/main/java/crabster/rudakov/sberschoollesson16hwk/TimerFragment.kt
+++ app/src/main/java/crabster/rudakov/sberschoollesson16hwk/TimerFragment.kt
@@ -1,7 +1,6 @@
 package crabster.rudakov.sberschoollesson16hwk
 
 import android.os.*
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,19 +8,12 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import crabster.rudakov.sberschoollesson16hwk.Constants.CURRENT_TIMER_READING
-import crabster.rudakov.sberschoollesson16hwk.Constants.LOG_TAG
 import crabster.rudakov.sberschoollesson16hwk.Constants.TOAST_MESSAGE
-import java.util.concurrent.*
 
 class TimerFragment : Fragment() {
 
     private var timerView: TextView? = null
-    private var timer: Int = 6
-
-    //Создаётся 'Executor' с 1 потоком
-    private var executor: ScheduledExecutorService = Executors.newScheduledThreadPool(1)
-    private lateinit var timerFuture: Future<out Any>
+    private var timer: Long = 10
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,37 +30,17 @@ class TimerFragment : Fragment() {
 
         val buttonStart = view.findViewById<Button>(R.id.button_start)
         buttonStart.setOnClickListener {
-            //передаётся значение таймера экзекутору
-            timerFuture = executor.scheduleAtFixedRate({
-                if (timer > 0) {
-                    timer--
-                    //Производится добавление сообщения с кодом метода 'updateTimer()' собственному 'Handler' TextView
-                    timerView?.post { updateTimer() }
-                } else {
-                    timerFuture.cancel(true)
+            object: CountDownTimer(timer * 1000, 1000) {
+
+                override fun onTick(millisUntilFinished: Long) {
+                    timerView?.text = (millisUntilFinished / 1000).toString()
                 }
-            }, 0, 1, TimeUnit.SECONDS)
+
+                override fun onFinish() {
+                    Toast.makeText(context, TOAST_MESSAGE, Toast.LENGTH_LONG).show()
+                }
+            }.start()
         }
-    }
-
-    /**
-     * Переопределяем данный метод с целью закрытия ресурсов по окончании работы приложения
-     * */
-    override fun onDestroyView() {
-        super.onDestroyView()
-        //Удаляются все работающие задачи
-        executor.shutdown()
-        //Зануляется ссылка на View для удаления её GC и высвобождения памяти
-        timerView = null
-    }
-
-    /**
-     * Метод устанавливает показание таймера в текстовое поле
-     * */
-    private fun updateTimer() {
-        Log.d(LOG_TAG, "${Thread.currentThread().name} - $CURRENT_TIMER_READING : $timer")
-        timerView?.text = timer.toString()
-        if (timer == 0) Toast.makeText(context, TOAST_MESSAGE, Toast.LENGTH_LONG).show()
     }
 
 }
